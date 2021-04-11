376844ebf143d34451ae02c302a3e7018d07bd08
thannedel <57347092+thannedel@users.noreply.github.com>
2021-04-11 11:11:12 +0200

    add repository for gameplayers

---
+++ src/main/java/com/codeoftheweb/salvo/Repositories/GamePlayerRepository.java
@@ -0,0 +1,9 @@
+package com.codeoftheweb.salvo.Repositories;
+
+import com.codeoftheweb.salvo.Entities.GamePlayer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface GamePlayerRepository extends JpaRepository<GamePlayer,Long> {
+}
