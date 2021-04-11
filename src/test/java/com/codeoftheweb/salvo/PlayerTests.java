841a0e3e978032a185539a7bea9e690edbc62964
thannedel <57347092+thannedel@users.noreply.github.com>
2021-04-11 11:12:33 +0200

    initial tests

---
+++ src/test/java/com/codeoftheweb/salvo/PlayerTests.java
@@ -0,0 +1,12 @@
+package com.codeoftheweb.salvo;
+
+import com.codeoftheweb.salvo.Entities.Player;
+import org.junit.jupiter.api.Test;
+
+@Test
+public class PlayerTests {
+
+        Player player = new Player();
+
+
+}
